8b5051d146ea0652ce5b30b8115876bafe2a1c71
111peri <perizat.melisova.kk@gmail.com>
2023-08-07 21:37:34 +0600

    add js null

---
+++ js/index.js
@@ -0,0 +1,69 @@
+// let num = 10;
+//    num = num + 1;
+//    num = num + 1;
+//    num = num - 1;
+//    console.log(num);
+
+// #1 
+
+// let num = 10
+
+// num++
+
+// console.log(num);
+
+// ++num
+
+// console.log(num)
+
+// num--
+
+// console.log(num)
+
+// #2
+
+// let num = 47;
+// num = num + 7;
+// num = num - 18;
+// num = num * 10;
+// num = num / 15;
+// alert(num);
+
+
+// const a = 47
+
+// const b = 7
+
+// const result = (((a + b) - 18) * 10) / 15
+
+// alert(result)
+
+
+// Переделайте приведенный код так, чтобы в нем использовались операции 
+//   +=, -=, *=, /=, ++, --. 
+//   Количество строк кода при этом не должно измениться. 
+//   Код для переделки:
+//   let num = 1;
+//   num = num + 12;
+//   num = num - 14;
+//   num = num * 5;
+//   num = num / 7;
+//   num = num + 1;
+//   num = num - 1;
+//   alert(num);
+
+let num = 1
+
+num += 12
+
+const resultSum = num
+
+num -= 14
+num *= 5
+num /= 7
+num++
+num--
+
+
+
+alert(num)
